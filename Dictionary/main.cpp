a538f85166352a561bd5095b6d9e6d36f4aa6379
Nahilor <nahomambachew16@gmail.com>
2025-05-04 18:20:11 +0100

    added button click event for search button

--- Dictionary/main.cpp
+++ Dictionary/main.cpp
@@ -1,64 +1,68 @@
 ﻿#include <wx/wx.h>
 
-// Define a new application type
+
+
 class MyApp : public wxApp
 {
 public:
     virtual bool OnInit();
 };
 
-// Define a new frame (window) type
 class MyFrame : public wxFrame {
 public:
+	
     MyFrame() : wxFrame(nullptr, wxID_ANY, "Dictionary", wxDefaultPosition, wxSize(400, 500))
     {
-        // Create a panel in the frame
         wxPanel* mainPnl = new wxPanel(this);
-        mainPnl->SetBackgroundColour(wxColour(230, 230, 230));  // Purple background
+        mainPnl->SetBackgroundColour(wxColour(230, 230, 230));
 
-        // --- Create controls ---
-        wxTextCtrl* searchBar = new wxTextCtrl(mainPnl, wxID_ANY, "Enter a word");
+        searchBar = new wxTextCtrl(mainPnl, wxID_ANY, "Enter a word");
         wxButton* searchBtn = new wxButton(mainPnl, wxID_ANY, "Search");
 
-        // --- Horizontal sizer for the search row ---
         wxBoxSizer* hbox = new wxBoxSizer(wxHORIZONTAL);
-        hbox->Add(searchBar, 1, wxEXPAND | wxRIGHT, 10);  // Stretchable textbox
-        hbox->Add(searchBtn, 0);  // Fixed-size button
+        hbox->Add(searchBar, 1, wxEXPAND | wxRIGHT, 10);  
+        hbox->Add(searchBtn, 0); 
 
-        // --- Vertical sizer for the whole panel ---
         wxBoxSizer* vbox = new wxBoxSizer(wxVERTICAL);
-        vbox->Add(hbox, 0, wxEXPAND | wxALL, 10);  // Add horizontal search bar
+        vbox->Add(hbox, 0, wxEXPAND | wxALL, 10);  
 
+		wxPanel* resultPanel = new wxPanel(mainPnl);
+		resultPanel->SetBackgroundColour(wxColour(255, 0, 255));
 
-        // --- Horizontal sizer for the search row ---
-        wxBoxSizer* f = new wxBoxSizer(wxHORIZONTAL);
-        f->Add(new wxStaticText(mainPnl, wxID_ANY, "Search for a word\n"), 1, wxEXPAND | wxRIGHT, 10);  // Stretchable textbox
+        vbox->Add(resultPanel, 1, wxEXPAND | wxALL, 10);
 
-        vbox->Add(f, 0, wxEXPAND | wxALL, 10);
-
-        // --- Set sizer on the panel ---
         mainPnl->SetSizer(vbox);
         mainPnl->Layout();
 
-        // --- Now put the panel into the frame ---
         wxBoxSizer* frameSizer = new wxBoxSizer(wxVERTICAL);
         frameSizer->Add(mainPnl, 1, wxEXPAND);
         this->SetSizer(frameSizer);
         this->Layout();
+
+        searchBtn->Bind(wxEVT_BUTTON, &MyFrame::OnSearch, this);
+
     }
+private:
+    wxTextCtrl* searchBar;
+    void OnSearch(wxCommandEvent& event);
 };
 
 
 
-// This macro tells wxWidgets to create the application object the main
 wxIMPLEMENT_APP(MyApp);
 
 
 
-// Initialize the application and create the main window
 bool MyApp::OnInit()
 {
     MyFrame* frame = new MyFrame();
-    frame->Show(true);  // Show the main window
+    frame->Show(true); 
     return true;
+}
+
+void MyFrame::OnSearch(wxCommandEvent& event)
+{
+	
+	wxString word = searchBar->GetValue();
+    wxMessageBox(word);
 }
